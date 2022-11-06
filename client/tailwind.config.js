adc7897c1e6979a063aceca8d91d509eb2b2a81d
Mohammed Abed <63855373+mabedd@users.noreply.github.com>
2022-11-06 21:28:24 +0300

    Finalize App

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{jsx,js}'
+    './src/**/*.{jsx,js}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
